f4913eea4a0e0c7f777bce4f338541a41eae7e26
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-11-13 21:39:46 +0100

    FEAT: Se remodela estructura archivos

--- src/main/java/com/serveat/repository/CategoriaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.serveat.repository;
-
-import com.serveat.domain.menu.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> { }
--- src/main/java/com/serveat/view/admin/GestionEmpleadosView.java
+++
@@ -1,2 +0,0 @@
-package com.serveat.view.admin;
-
