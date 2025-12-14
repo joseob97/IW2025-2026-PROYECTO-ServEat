130e7afb4570d4045e697c9e30a6f4763dbb05a3
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-12-14 13:16:37 +0100

    FIX: SONAR mantener CSRF activo y evitar csrf.disable()

--- src/main/java/com/serveat/core/config/security/SecurityConfig.java
+++ src/main/java/com/serveat/core/config/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+                // CSRF activado por defecto (no lo desactivamos)
 
                 // RUTAS PÚBLICAS
                 .authorizeHttpRequests(auth -> auth
