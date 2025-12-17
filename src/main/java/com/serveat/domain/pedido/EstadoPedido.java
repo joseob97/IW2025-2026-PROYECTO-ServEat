d351c55a17876274977c967653c1df5c2f2c1a6a
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-12-17 17:05:20 +0100

    FEAT: Modificación del enum de estado pedido, para independencia

--- src/main/java/com/serveat/domain/pedido/EstadoPedido.java
+++ src/main/java/com/serveat/domain/pedido/EstadoPedido.java
@@ -1,11 +1,7 @@
 package com.serveat.domain.pedido;
 
 public enum EstadoPedido {
-
-    EN_CURSO,      // Camarero creando / editando
-    EN_COCINA,     // Confirmado → cocina lo ve
-    EN_PREPARACION,// Cocinero empieza a cocinar
-    LISTO,         // Cocina termina
-    SERVIDO,       // Camarero entrega
-    CANCELADO
+    EN_CURSO,
+    EN_COCINA,
+    ANULADO
 }
