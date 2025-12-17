ac3af1234b12dcdbadb93add611db276692c1db1
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-12-17 17:03:54 +0100

    FEAT: Enum de estados para los cocineros

---
+++ src/main/java/com/serveat/domain/pedido/EstadoCocina.java
@@ -0,0 +1,8 @@
+package com.serveat.domain.pedido;
+
+public enum EstadoCocina {
+    PENDIENTE_ACEPTACION,  // camarero confirmó, cocina aún no lo ha aceptado
+    ACEPTADO,              // cocina lo acepta (ya no se puede cancelar/rectificar)
+    EN_PREPARACION,
+    LISTO
+}
