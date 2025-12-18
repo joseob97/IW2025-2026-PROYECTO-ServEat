635dbccdb4195e9703af0f9594ba2d83d7e831af
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-12-18 16:14:43 +0100

    FEAT: Se añaden el repository para el pago

---
+++ src/main/java/com/serveat/repository/pago/PagoRepository.java
@@ -0,0 +1,11 @@
+package com.serveat.repository.pago;
+
+import com.serveat.domain.pago.Pago;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PagoRepository extends JpaRepository<Pago, Long> {
+
+    Optional<Pago> findByPedido_Codigo(String codigoPedido);
+}
