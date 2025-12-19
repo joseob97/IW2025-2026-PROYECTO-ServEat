ac2b78052ef0d27f6d63382655e8b7d5e2d95d06
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-12-19 16:01:31 +0100

    FEAT: Implementación de métodos para el servicio cliente

---
+++ src/main/java/com/serveat/service/usuario/impl/ClienteServiceImpl.java
@@ -0,0 +1,30 @@
+package com.serveat.service.usuario.impl;
+
+import com.serveat.domain.usuario.Cliente;
+import com.serveat.repository.usuario.ClienteRepository;
+import com.serveat.service.usuario.ClienteService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class ClienteServiceImpl implements ClienteService {
+
+    private final ClienteRepository clienteRepo;
+
+    public ClienteServiceImpl(ClienteRepository clienteRepo) {
+        this.clienteRepo = clienteRepo;
+    }
+
+    @Override
+    public Cliente obtenerPorUsername(String username) {
+        return clienteRepo.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("Cliente no encontrado"));
+    }
+
+    @Override
+    public Cliente obtenerConPedidosPorUsername(String username) {
+        return clienteRepo.findWithPedidosByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("Cliente no encontrado"));
+    }
+}
