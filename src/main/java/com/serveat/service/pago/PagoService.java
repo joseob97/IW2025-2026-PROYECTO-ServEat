a3b54bba362f3a36905486a48afaa10d50c87b3e
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-12-22 15:17:24 +0100

    FEAT: Declaración de métodos para realizar el pago en el servicio pago

--- src/main/java/com/serveat/service/pago/PagoService.java
+++ src/main/java/com/serveat/service/pago/PagoService.java
@@ -4,6 +4,8 @@ import com.serveat.domain.pago.MetodoPago;
 import com.serveat.domain.pago.Pago;
 import com.serveat.domain.pedido.Pedido;
 
+import java.math.BigDecimal;
+
 public interface PagoService {
 
     Pago iniciarPago(Pedido pedido, MetodoPago metodo);
@@ -11,4 +13,14 @@ public interface PagoService {
     Pago confirmarPago(Long pagoId, String referencia);
 
     void marcarPagoFallido(Long pagoId, String motivo);
+
+    Pago procesarPagoOnline(Pedido pedidoCreado,
+                            MetodoPago metodo,
+                            String cardNumber,
+                            String cardHolder,
+                            String cardExpiryMMYY,
+                            String cardCvv,
+                            String paypalEmail,
+                            String paypalPassword,
+                            BigDecimal efectivoPagaCon);
 }
