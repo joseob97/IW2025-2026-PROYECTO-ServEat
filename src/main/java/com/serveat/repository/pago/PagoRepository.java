b1c715eb995a036e71a7db1857d5fa887d39eee0
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-12-23 19:18:22 +0100

    FEAT: Métodos relacionados con el pago que se usaran en el servicio de estadísticas

--- src/main/java/com/serveat/repository/pago/PagoRepository.java
+++ src/main/java/com/serveat/repository/pago/PagoRepository.java
@@ -1,9 +1,11 @@
 package com.serveat.repository.pago;
 
+import com.serveat.domain.pago.EstadoPago;
 import com.serveat.domain.pago.Pago;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PagoRepository extends JpaRepository<Pago, Long> {
@@ -12,4 +14,10 @@ public interface PagoRepository extends JpaRepository<Pago, Long> {
 
     @EntityGraph(attributePaths = {"pedido", "pedido.cliente"})
     Optional<Pago> findWithPedidoById(Long id);
+
+    // Para las estadísticas (facturación)
+    long countByEstado(EstadoPago estado);
+
+    @EntityGraph(attributePaths = {"pedido"})
+    List<Pago> findByEstado(EstadoPago estado);
 }
