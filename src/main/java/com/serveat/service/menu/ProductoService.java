bd27411fc44f367cf3be311b94a450e9edcf48d5
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-12-26 13:57:41 +0100

    FEAT: Interfaz del servicio de producto con nuevos métodos para los ingredientes

--- src/main/java/com/serveat/service/menu/ProductoService.java
+++ src/main/java/com/serveat/service/menu/ProductoService.java
@@ -1,14 +1,41 @@
 package com.serveat.service.menu;
 
 import com.serveat.domain.menu.Producto;
+
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Set;
 
 public interface ProductoService {
 
     Producto crearProducto(String nombre, String descripcion, BigDecimal precio, String categoriaNombre);
 
+    Producto crearProductoConIngredientes(
+            String nombre,
+            String descripcion,
+            BigDecimal precio,
+            String categoriaNombre,
+            Set<String> ingredientesSeleccionados,
+            byte[] imagenBytes,
+            String nombreArchivo
+    );
+
+    Producto actualizarProductoConIngredientes(
+            String codigo,
+            String nombre,
+            String descripcion,
+            BigDecimal precio,
+            String categoriaNombre,
+            Set<String> ingredientesSeleccionados,
+            byte[] imagenBytes,
+            String nombreArchivo
+    );
+
     Producto obtenerPorCodigo(String codigo);
+
+    Producto obtenerConIngredientesPorCodigo(String codigo);
+
+    List<Producto> listarProductos();
 
     List<Producto> buscarPorCategoria(String categoriaNombre);
 
@@ -17,4 +44,6 @@ public interface ProductoService {
     List<Producto> buscarPorDescripcionParcial(String descripcion);
 
     void eliminarProducto(String codigo);
+
+    List<String> listarNombresIngredientes();
 }
