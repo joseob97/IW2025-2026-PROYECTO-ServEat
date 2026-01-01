3b4e0e8810d41196f70bdc4afa59a60bb24997a4
ajseiva <alvaro.jaenseiva@alum.uca.es>
2026-01-01 18:15:21 +0100

    TEST: Modificación de archivos para solucionar error en tests

--- src/test/java/com/serveat/ServEatApplicationTests.java
+++ src/test/java/com/serveat/ServEatApplicationTests.java
@@ -4,11 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:serveat_test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=MySQL",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 @ActiveProfiles("test")
 class ServEatApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() {}
 }
